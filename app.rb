ca334498b8cac9b32d5236314651aceae2b6a526
Gabe <gabepopa9195@gmail.com>
2016-04-18 16:26:41 -0400

    updated files as of 4-17-2016

--- app.rb
+++ app.rb
@@ -1,91 +1,29 @@
+require 'pathname'
+require 'pg'
+require 'logger'
+require 'yaml'
+require 'rubygems'
+require 'bundler/setup'
 require 'sinatra'
+require "sinatra/activerecord"
 
+require 'erb'
 
+APP_ROOT = Pathname.new(File.expand_path(File.dirname(__FILE__)))
 
-def get_birth_path_num(birthdate)
+Dir[APP_ROOT.join('app', 'controllers', '*.rb')].sort{|a,b| !a.include?("index") ? 1 : -1 <=> b.include?("index") ? -1 : 1}.each { |f| require f }
 
+Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |f| require f }
 
-number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
+ActiveRecord::Base.logger = Logger.new(STDOUT)
 
-number = number.to_s
-number = number[0].to_i + number[1].to_i
-
-
-if number > 9
-
-	number = number.to_s
-
-        number = number[0].to_i + number[1].to_i
-end
-return number
-
+Dir[APP_ROOT.join('app/models', '*.rb')].each do |model_file|
+  filename = File.basename(model_file).gsub('.rb', '')
+  autoload ActiveSupport::Inflector.camelize(filename), model_file
 end
 
+helpers ApplicationHelper
 
-def get_message(birth_path_num)
-
-  case birth_path_num
-
-  when 1
-
-    message = "Your numerology number is #{birth_path_num}.\nOne is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
-
-  when 2
-
-    message = "Your numerology number is #{birth_path_num}.\nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
-
-  when 3
-
-    message = "Your numerology number is #{birth_path_num}.\nNumber Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
-
-  when 4
-
-    message = "Your numerology number is #{birth_path_num}.\nThis is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
-
-  when 5
-
-    message = "Your numerology number is #{birth_path_num}.\nThis is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
-
-  when 6
-
-    message = "Your numerology number is #{birth_path_num}.\nThis is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
-
-  when 7
-
-    message = "Your numerology number is #{birth_path_num}.\nThis is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
-
-  when 8
-
-    message = "Your numerology number is #{birth_path_num}.\nThis is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
-
-  when 9
-
-    message = "Your numerology number is #{birth_path_num}.\nThis is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
-
-  else
-
-    message = "Uh oh! Your birth path number is not 1-9!"
-
-end
-
-end
-
-
-get '/messages/' do
-	erb :messages
-end
-
-
-
-get '/:birthdate' do
-birthdate = params[:birthdate]
-
-birth_path_num = get_birth_path_num(birthdate)
-
-@message = get_message(birth_path_num)
-erb :index
-
-end
 
 
 
